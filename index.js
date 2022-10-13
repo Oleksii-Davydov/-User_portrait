f52abb62f5cffa76514ca6e74c8c5020be82a073
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-13 13:20:14 +0300

    add result_1/2/3, add \n

--- index.js
+++ index.js
@@ -2,39 +2,32 @@ const yearOfBirth = prompt("Enter your year of birth");
 const city = prompt("What city do you live in?");
 const sport = prompt("What is your favorite kind of sport ");
 
-let result_1 = yearOfBirth
 if (yearOfBirth) {
-    alert (2022 - (+yearOfBirth))
+    result_1 = (2022 - (+yearOfBirth))
+} else {
+    result_1 = ('It is a pity that you did not want to enter your year of birth')
 }
-else {
-    alert('It is a pity that you did not want to enter your year of birth')
-}
-let result_2 = city
 if (city === 'Kyiv') {
-    alert("You live in the capital of Ukraine")
-} else
-    if (city === 'London') {
-    alert("You live in the capital of Great Britain")
-} else
-    if (city === 'Washington') {
-    alert("You live in the capital of Great USA")
+    result_2 = ("You live in the capital of Ukraine")
+} else if (city === 'London') {
+    result_2 = ("You live in the capital of Great Britain")
+} else if (city === 'Washington') {
+    result_2 = ("You live in the capital of Great USA")
 
 } else if (city) {
-    alert("You live in the city of " + city)
-}
-    else {
-        alert("It is a pity that you did not want to enter the city in which you live:((")
-    }
-    let result_3 = sport
-if (sport === 'football') {
-    alert("Cool! Do you want to be Messi?")
-} else if (sport === 'box') {
-    alert("Cool! Do you want to be Tyson?")
-} else if (sport === 'tennis') {
-    alert("Cool! Do you want to be Nadal?")
+    result_2 = ("You live in the city of " + city)
 } else {
-    alert("It's a pity that you didn't want to enter your favorite sport")
+    result_2 = ("It is a pity that you did not want to enter the city in which you live:((")
 }
-const result = (result_1+result_2+result_3)
+if (sport === 'football') {
+    result_3 = ("Cool! Do you want to be Messi?")
+} else if (sport === 'box') {
+    result_3 = ("Cool! Do you want to be Tyson?")
+} else if (sport === 'tennis') {
+    result_3 = ("Cool! Do you want to be Nadal?")
+} else {
+    result_3 = ("It's a pity that you didn't want to enter your favorite sport")
+}
+const result = (result_1+"\n" + result_2 +"\n"+ result_3)
 alert(result)
 
