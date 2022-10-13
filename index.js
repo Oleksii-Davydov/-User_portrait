c0d8fd806daf67d60281f8a6f2f70b4d5802488a
Oleksii-Davydov <davydovalekcey1992@gmail.com>
2022-10-13 16:36:50 +0300

    add ... ===null(3). Finish

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const yearOfBirth = prompt("Enter your year of birth");
-const city = prompt("What city do you live in?");
-const sport = prompt("What is your favorite kind of sport ");
-
-if (yearOfBirth) {
-    result_1 = (2022 - (+yearOfBirth))
+if (yearOfBirth === null) {
+    alert("It is a pity that you did not want to enter your year of birth.");
 } else {
-    result_1 = ('It is a pity that you did not want to enter your year of birth')
-}
-if (city === 'Kyiv') {
-    result_2 = ("You live in the capital of Ukraine")
-} else if (city === 'London') {
-    result_2 = ("You live in the capital of Great Britain")
-} else if (city === 'Washington') {
-    result_2 = ("You live in the capital of Great USA")
+    const city = prompt("What city do you live in?");
+    if (city === null) {
+        alert("It is a pity that you did not want to enter the city in which you live:((");
+    } else {
+        const sport = prompt("What is your favorite kind of sport");
+        if (sport === null) {
+            alert("It's a pity that you didn't want to enter your favorite sport.");
+        } else {
+            if (yearOfBirth) {
+                result_1 = "You are " + (2022 - parseInt(yearOfBirth)) + "old.";
+            }
+            if (city === 'Kyiv') {
+                result_2 = "You live in the capital of Ukraine.";
+            } else if (city === 'London') {
+                result_2 = "You live in the capital of Great Britain.";
+            } else if (city === 'Washington') {
+                result_2 = "You live in the capital of Great USA.";
+            } else if (city) {
+                result_2 = "You live in the city of " + city;
+            }
+            if (sport === 'football') {
+                result_3 = "Cool! Do you want to be Messi?";
+            } else if (sport === 'box') {
+                result_3 = "Cool! Do you want to be Tyson?";
+            } else if (sport === 'tennis') {
+                result_3 = "Cool! Do you want to be Nadal?";
+            }
+            const result = (result_1 + "\n" + result_2 + "\n" + result_3)
+            alert(result)
+        }
+    }
 
-} else if (city) {
-    result_2 = ("You live in the city of " + city)
-} else {
-    result_2 = ("It is a pity that you did not want to enter the city in which you live:((")
 }
-if (sport === 'football') {
-    result_3 = ("Cool! Do you want to be Messi?")
-} else if (sport === 'box') {
-    result_3 = ("Cool! Do you want to be Tyson?")
-} else if (sport === 'tennis') {
-    result_3 = ("Cool! Do you want to be Nadal?")
-} else {
-    result_3 = ("It's a pity that you didn't want to enter your favorite sport")
-}
-const result = (result_1+"\n" + result_2 +"\n"+ result_3)
-alert(result)
-
